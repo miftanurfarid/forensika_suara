a9d08836fc9d6d2ada60a720d925de94b376f144
miftanurfarid <miftanurfarid@gmail.com>
2021-09-03 02:39:15 +0800

    pertemuan 2 - konsep bunyi

--- slide/01.bunyi/code/waveform_dan_spektrum.py
+++ slide/01.bunyi/code/waveform_dan_spektrum.py
@@ -28,3 +28,23 @@ plt.title('Sine wave f='+str(f)+' Hz') # plot title
 plt.xlabel('Time (s)') # x-axis label
 plt.ylabel('Amplitude') # y-axis label
 plt.show() # display the figure
+
+"""
+FFT plot – Absolute frequency on the x-axis vs. magnitude on y-axis:
+"""
+
+from scipy.fftpack import fft,fftshift
+
+NFFT=1024     
+X=fftshift(fft(x,NFFT))
+
+fig4, ax = plt.subplots(nrows=1, ncols=1) #create figure handle
+
+fVals=np.arange(start = -NFFT/2,stop = NFFT/2)*fs/NFFT
+ax.plot(fVals,np.abs(X),'b')
+ax.set_title('Double Sided FFT - with FFTShift')
+ax.set_xlabel('Frequency (Hz)')         
+ax.set_ylabel('|DFT Values|')
+ax.set_xlim(-50,50)
+ax.set_xticks(np.arange(-50, 50+10,10))
+fig4.show()
